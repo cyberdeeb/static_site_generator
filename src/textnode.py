10834fad2a646026738bd133e8db408d7822c9fa
cyberdeeb <abrahamdeeb@gmail.com>
2024-12-27 10:49:59 -0800

    added textnode methods

--- src/textnode.py
+++ src/textnode.py
@@ -9,9 +9,20 @@ class TextType(Enum):
     IMAGES = 'images'
 
 class TextNode():
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type: TextType, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
+    def __eq__(self, other_textnode):
+        if isinstance(other_textnode, TextNode):
+            return (
+                self.text == other_textnode.text and 
+                self.text_type == other_textnode.text_type and 
+                self.url == other_textnode.url
+                )
+        return False
+
+    def __repr__(self):
+        return f'TextNode({self.text!r}, {self.text_type.value!r}, {self.url!r})'
 
