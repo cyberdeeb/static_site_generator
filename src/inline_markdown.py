fd86fab5ccdef7c4173889f0265f2597a3de431c
cyberdeeb <abrahamdeeb@gmail.com>
2025-01-07 09:33:14 -0800

    Fixed bug in inline markdown & added extraction tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -13,11 +13,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if node.text_type.value == 'text':
             split_text = node.text.split(delimiter)
             for i, text in enumerate(split_text):
-
-                if i == 1:
-                    new_nodes.append(TextNode(text, text_type))
-                else:
-                    new_nodes.append(TextNode(text, TextType.TEXT))
+                if text:
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(text, TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(text, text_type))
 
         else:
             new_nodes.append(node)
